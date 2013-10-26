96e796a1b83484cbd3f3b26892af30d9f617c5d4
richard <richardflett@gmail.com>
2013-10-27 01:39:13 +0200

    receiving the song list

--- js/main.js
+++ js/main.js
@@ -1,53 +1,44 @@
 (function ($) {
 
-    var songs = [{artist:"Artist A", title:"Song A", album:"Album A", length:"123"},
-        {artist:"Artist B", title:"Song B", album:"Album B", length:"132"},
-        {artist:"Artist C", title:"Song C", album:"Album C", length:"321"},
-        {artist:"Artist D", title:"Song D", album:"Album D", length:"213"},
-       ];
-
-
     var Song = Backbone.Model.extend({
-        defaults:{
-            artist:"Artist A",
-            title:"Title A",
-            album:"Album A",
-            length:"1"      
-        }
+        
     });
 
-    var uuid = 'beatcoin';// change with subdomain.
+
     var Jukebox = Backbone.Collection.extend({
        model:Song,
-       url:'http://engine.beatcoin.org/jukebox/526c1ed6eb63e7ebe22dabe6/songs'
-      // url:'beatcoin/jukebox/' + uuid + '/queue' // change this when it is all go
+       url:'http://engine.beatcoin.org/jukebox/526c1ed6eb63e7ebe22dabe6/songs',
+
+       parse:function(response){
+            return response.items;
+       }
+
     });
 
     var SongView = Backbone.View.extend({
         tagName:"div",
         className:"songContainer",
-        template:$("#songTemplate").html(),
-
+        
         render:function () {
-            var tmpl = _.template(this.template); 
-
+            var tmpl = _.template($("#songTemplate").html()); 
             this.$el.html(tmpl(this.model.toJSON())); 
             return this;
         }
+
     });
 
     var JukeboxView = Backbone.View.extend({
         el:$("#songs"),
 
-        initialize:function(){Jukebox
-            this.collection = new Jukebox(songs);
-            this.collection.fetch();
-            this.render();
+        initialize:function(){
+            this.collection.on("change reset add remove", this.renderSong, this);
         },
 
         render: function(){
             var that = this;
+           
             _.each(this.collection.models, function(item){
+             
                 that.renderSong(item);
             }, this);
         },
@@ -60,7 +51,9 @@
         }
     });
 
-    var jukeboxView = new JukeboxView();
+    var jukebox = new Jukebox();
 
+    var jukeboxView = new JukeboxView({collection:jukebox});
+        jukebox.fetch();
 
 })(jQuery);
